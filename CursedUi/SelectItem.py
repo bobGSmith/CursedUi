ab914f9b1505486b601f8145386c2a9155856074
Bob G Smith <bobbyatopk@gmail.com>
2023-06-02 12:37:28 +0100

    select item scrolling

--- CursedUi/SelectItem.py
+++ CursedUi/SelectItem.py
@@ -15,6 +15,7 @@ class SelectItem () :
         self.width = label_width + item_width + 2
         self.label_width = label_width 
         self.item_width = item_width
+        self.index = 0
         self.window = containerScr.subwin(0, self.width, self.y, self.x + shift)
         self.selection = 0
         self.active = False
@@ -23,7 +24,7 @@ class SelectItem () :
         self.build()
     def build (self):  
         self.window.clear() 
-        self.window.addstr(0,0,f"{self.label:<{self.label_width}} : {self.items[self.selection]:<{self.item_width}}",self.attr)
+        self.window.addstr(0,0,f"{self.label:<{self.label_width}} : {self.getTruncatedValue(self.items[self.selection],self.index):<{self.item_width}}",self.attr)
         self.window.refresh()
     def get_choice(self, key):
         if self.active: 
@@ -31,16 +32,22 @@ class SelectItem () :
                 self._next()
             if key == curses.KEY_DOWN:
                 self._previous()
+            if key == curses.KEY_RIGHT:
+                self.index += 1 
+                self.build()
     def _next (self): 
         self.selection = (self.selection + 1) % len(self.items) 
+        self.index = 0
         self.build()
     def _previous (self): 
         self.selection -= 1
         if self.selection < 0: self.selection = len(self.items) - 1
+        self.index = 0
         self.build()
     def activate (self):
         self.active = True
         self.attr = self.selected_attr
+        self.index = 0
         self.build()
         if self.hint_box != None: 
             self.hint_box.clear() 
@@ -54,3 +61,14 @@ class SelectItem () :
         self.build()
     def returnChoice(self):
         return self.items[self.selection]
+    def getTruncatedValue (self,index=0,trailing_start="",trailing_end = ".."):
+        value = self.items[self.selection]
+        i = index % len(value)
+        if len(value) < self.item_width: 
+            return value 
+        else: 
+            truncated = f"{value[i:(i+(self.item_width) if len(value) - i > self.item_width else None)]}"
+            if len(truncated) < self.item_width:
+                return getTruncatedValue(f"{truncated} {value}",self.item_width)
+            else:
+                return f"{trailing_start}{truncated[len(trailing_start):-len(trailing_end)]}{trailing_end}"
